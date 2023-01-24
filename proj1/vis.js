5065e117802402d914c54971e0d3996fa61f9d5e
tiagombp <tiagombp@gmail.com>
2023-01-24 06:16:58 +0100

    adds basic styling, creates control buttons

--- proj1/vis.js
+++ proj1/vis.js
@@ -10,9 +10,31 @@ function init() {
 
             console.log(skills);
 
+            const controls = new Controls(skills);
+
         }
     )
 
 }
 
 init();
+
+class Controls {
+
+    constructor(skills) {
+
+        const cont = document.querySelector('.skills-container');
+
+        skills.forEach(skill => {
+
+            const new_btn = document.createElement('button');
+            new_btn.innerText = skill;
+            new_btn.classList.add('skill-button');
+
+            cont.appendChild(new_btn);
+
+        })
+
+    }
+
+}
